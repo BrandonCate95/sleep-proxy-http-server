7841ef688658aab82c9e793c74ba0de439b27fe3
Brandon Cate <brandoncate95@gmail.com>
2020-03-18 16:33:23 -0700

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http')
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
